75315f840402e0ad66b82645699633fb37e15084
zilleplus <zillepus@gmail.com>
2022-07-21 16:45:37 +0200

    Add nnkerasvalue function
    * Added save callback to training so the nn gets saved in .keras file.
    * Added NNKerasValueFunction, a valuefunction that uses the saved .keras
      file.

--- min_max_engine/mmEngine/value_funtions/__init__.py
+++ min_max_engine/mmEngine/value_funtions/__init__.py
@@ -1,3 +1,3 @@
 from .material import ValueFunctionMaterial, MaterialCount
-from .nn_keras import CreateModel, TrainModel, NNValueFunction
+from .nn_keras import CreateModel, TrainModel, NNKerasValueFunction, LoadModel
 from .value_function import ValueFunction
--- min_max_engine/mmEngine/value_funtions/nn_keras.py
+++ min_max_engine/mmEngine/value_funtions/nn_keras.py
@@ -7,7 +7,11 @@ import tensorflow.keras.layers as layers  # type: ignore
 import numpy as np
 from typing import Optional
 from pathlib import Path
+from mmEngine.database import convert
 
+def LoadModel(file_location: Path):
+    assert file_location.exists()
+    return keras.models.load_model(file_location)
 
 def CreateModel() -> keras.Model:
     # 8*8 board -> one hot encoding of 12 states
@@ -37,6 +41,7 @@ def CreateModel() -> keras.Model:
         layers.Dropout(0.25),
         layers.Conv2D(filters=128, kernel_size=1, padding="same", activation="relu"),
 
+        layers.Flatten(),
         layers.Dense(units=1, activation="tanh")
         ])
 
@@ -51,8 +56,12 @@ def CreateModel() -> keras.Model:
     return model
 
 
-def TrainModel(model: keras.Model, dataset: np.ndarray, log_dir_board: Optional[Path]):
-    batch_size: int = 32
+def TrainModel(model: keras.Model, 
+               dataset: np.ndarray,
+               save_path: Optional[Path] = None,
+               log_dir_board: Optional[Path] = None,
+               print_gradients: bool = False):
+    batch_size: int = 1024
 
     X = \
         tf.data.Dataset.from_tensors(dataset["X"]) \
@@ -70,20 +79,29 @@ def TrainModel(model: keras.Model, dataset: np.ndarray, log_dir_board: Optional[
         .unbatch()
 
     data = tf.data.Dataset.zip((X_one_hot, Y))\
+        .shuffle(10000) \
         .batch(batch_size=batch_size)
-        #.shuffle(10000) \
 
     # Take 1000 batches of data as validation set.
     # The shuffle made sure there spread out from
     # all kings of games.
-    validation_size = 10
+    validation_size = 1000
     validation_data = data.take(validation_size)
-    train_data = data.skip(validation_size).take(validation_size*10)
+    train_data = data.skip(validation_size)#.take(validation_size*10)
 
     callbacks: list = []
 
-    # if log_dir_board is not None:
-    #     callbacks.append(keras.callbacks.TensorBoard(log_dir=log_dir_board))
+    if save_path is not None:
+        print(f"Adding save callback to location: {save_path}")
+        callback_save = keras.callbacks.ModelCheckpoint(
+            filepath=save_path,
+            minotor="val_loss",
+            save_best_only=True)
+        callbacks.append(callback_save)
+
+
+    if log_dir_board is not None:
+        callbacks.append(keras.callbacks.TensorBoard(log_dir=log_dir_board))
 
     class GradCallback(keras.callbacks.Callback):
         model: keras.Model
@@ -98,14 +116,30 @@ def TrainModel(model: keras.Model, dataset: np.ndarray, log_dir_board: Optional[
             grad = tape.gradient(loss, model.trainable_weights)
             print(grad)
 
-    # enable this if you want to see the gradients.
-    # callbacks.append(GradCallback(model))
+    if print_gradients:
+        callbacks.append(GradCallback(model))
 
     model.fit(x=train_data, validation_data=validation_data, epochs=100, callbacks=callbacks)
 
     return
 
 
-class NNValueFunction(ValueFunction):
-    def __call__(self, board: Board):
-        ...
+class NNKerasValueFunction(ValueFunction):
+    def __init__(self, keras_model_location: Path):
+        self.model = LoadModel(keras_model_location)
+
+    def __call__(self, board: Board) -> float:
+        tf_board = tf.data.Dataset.from_tensors(convert(board))
+        tf_board = tf_board \
+            .map(lambda x: tf.reshape(x, (8, 8))) \
+            .map(lambda x: tf.one_hot(x, 12)) \
+            .map(lambda x: tf.cast(x, tf.float32)) \
+            .batch(1)
+
+        tensor = next(iter(tf_board))
+        pred = self.model(tensor, training=False)
+
+        if board.turn:
+            return float(pred)
+        else:
+            return -float(pred)
